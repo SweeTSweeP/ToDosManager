e59adeb03c0ce7802d58983cdda5d4d9b581dd2e
Ruslan <Ruslan.Lomaka@Gmail.com>
2020-04-04 18:27:48 +0300

    Controller completion

--- ToDosManagerBackSide/ToDosManagerBackSide/Controllers/ToDoController.cs
+++ ToDosManagerBackSide/ToDosManagerBackSide/Controllers/ToDoController.cs
@@ -9,23 +9,23 @@ using ToDosManagerBackSide.Domain.Entities;
 namespace ToDosManagerBackSide.Controllers
 {
     [ApiController]
-    [Route("ToDo")]
+    [Route("todo")]
     public class ToDoController : Controller
     {
         private ToDosManagerDbContext _dbContext;
-
+        
         public ToDoController(ToDosManagerDbContext context)
         {
             _dbContext = context;
         }
 
-        [HttpGet("Tasks")]
+        [HttpGet("~/todo/tasks")]
         public IEnumerable<Record> GetTasks()
         {
             return _dbContext.Tasks.ToList();
         }
 
-        [HttpGet("ToDo/{date}")]
+        [HttpGet("~/todo/tasks/{date}")]
         public IEnumerable<Record> GetTaskById(DateTime TasksDay)
         {
             var query = _dbContext.Days.FirstOrDefault(s => s.TasksDay == TasksDay);
@@ -40,12 +40,40 @@ namespace ToDosManagerBackSide.Controllers
             }
         }
 
-        [HttpPost("ToDo")]
+        [HttpPost("~/todo/tasks")]
         public void AddTasksDay(Day TasksDay)
         {
             _dbContext.Add(TasksDay);
         }
-        
-        
+
+        [HttpDelete("~/todo/tasks/{date}")]
+        public void DeleteTasksDay(Day TasksDay)
+        {
+            _dbContext.Remove(TasksDay);
+        }
+
+        [HttpPost("~/todo/tasks/{date}")]
+        public void AddTask(Record Task)
+        {
+            _dbContext.Add(Task);
+        }
+
+        [HttpPut("~/todo/tasks/{date}/{taskId}")]
+        public void UpdateTask(int TaskId, Record Task)
+        {
+            var Record = _dbContext.Tasks.Find(TaskId);
+
+            Record.Status = Task.Status;
+            Record.Description = Task.Description;
+            Record.Header = Task.Header;
+        }
+
+        [HttpDelete("~/todo/tasks/{date}/{taskId}")]
+        public void DeleteTask(int TaskId)
+        {
+            var Record = _dbContext.Days.Find(TaskId);
+
+            _dbContext.Remove(Record);
+        }
     }
 }
